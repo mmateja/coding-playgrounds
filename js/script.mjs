805264c165f2c00302cd4002ae8f83dff50946bc
Marek Mateja <matejowy@gmail.com>
2019-07-31 22:14:15 +0200

    Change machine implementation using Ramda

--- js/script.mjs
+++ js/script.mjs
@@ -1,3 +1,22 @@
 import R from 'ramda';
 
-console.log('Hello World!')
+const x = {
+    coinsNumber: 0,
+    amountRemaining: 33
+}
+
+const nominals = [25, 10, 5, 1]
+
+const answer = R.reduce(
+    (acc, nominal) => {
+        const nominalCoins = Math.floor(acc.amountRemaining / nominal)
+        return nominalCoins > 0
+        ? {
+            coinsNumber: acc.coinsNumber + nominalCoins,
+            amountRemaining: acc.amountRemaining - nominalCoins * nominal
+        }
+        : acc
+    },
+    x,
+    nominals
+)
